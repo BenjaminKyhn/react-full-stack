bde2bee4d3b94d6ba41b9c60609da1a274768583
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-17 12:16:53 +0100

    4-6 complete

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -3,6 +3,7 @@ import articleContent from '../pages/article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsLists";
+import UpvotesSection from "../components/UpvotesSection";
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
@@ -26,7 +27,7 @@ const ArticlePage = ({ match }) => {
     return (
         <Fragment>
             <h1>{article.title}</h1>
-            <p>This post has been upvoted {articleInfo.upvotes} times</p>
+            <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
