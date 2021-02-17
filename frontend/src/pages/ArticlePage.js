45f6efa4f768f985553cfb27155d98dd2fd05f0d
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-17 12:05:33 +0100

    4-4 complete

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import articleContent from '../pages/article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from "./NotFoundPage";
@@ -7,6 +7,17 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData()
+    }, [name])
+
     if (!article) return <NotFoundPage/>
 
     const otherArticles = articleContent.filter(article => article.name !== name)
@@ -14,6 +25,7 @@ const ArticlePage = ({ match }) => {
     return (
         <Fragment>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
